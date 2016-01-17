ab3b21378ab1535df00c6cb0a40ec7c92f718877
Riyaaaaa <s113111@edu.asahikawa-nct.ac.jp>
2016-01-18 02:21:12 +0900

    [Other]Changed the namespace structure belongs

--- Structure.h
+++ Structure.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#include<vector>
+
+namespace mtl{
+
 struct Size{
 	Size() = default;
 	Size(float w, float h) :width(w), height(h) {}
 	float width, height;
 };
+
+typedef std::vector< std::vector<float> > Map;
+
+}
